3ca8bafc4fc725a8413c3c447ecb9a7ca12255da
GauravKumarGR8 <bsn.gauravkr1@gmail.com>
2021-10-01 13:04:23 +0530

    used conditional expression

--- src/App.js
+++ src/App.js
@@ -7,21 +7,21 @@ import React, { useState } from 'react'
 function App() {
   const [mode, setMode] = useState('light') // To change mode between dark and light
 
-  const [color, setColor] = useState('dark')
+  // const [color, setColor] = useState('dark')
 
   const toggleMode = ()=>{
     if(mode === 'light'){
       setMode('dark')
-      setColor('light')
+      // setColor('light')
     }
     else{
       setMode('light')
-      setColor('dark')
+      // setColor('dark')
     }
   }
   return (
     <>
-    <Navbar title = 'TextUtils' aboutText = 'About Us' mode={mode} toggleMode={toggleMode} modeTitle={color}/>
+    <Navbar title = 'TextUtils' aboutText = 'About Us' mode={mode} toggleMode={toggleMode} /*modeTitle={color}*/ />
     {/* <Navbar/> */}
     <div className="container my-3" >
     <TextForm heading='Enter the text to analyze below'/>
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -22,7 +22,8 @@ export default function Navbar(props) {
                     <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search" />
                     <button className="btn btn-outline-info" type="submit">Search</button>
                 </form>
-                <div className={`form-check form-switch mx-3 text-${props.modeTitle}`}>
+                {/* <div className={`form-check form-switch mx-3 text-${props.modeTitle}`}> */}
+                <div className={`form-check form-switch mx-3 text-${props.mode==='light'?'dark':'light'}`}>
                     <input className="form-check-input" onClick={props.toggleMode} type="checkbox" id="flexSwitchCheckDefault" />
                     <label className="form-check-label" htmlFor="flexSwitchCheckDefault">Dark Mode</label>
                 </div>
