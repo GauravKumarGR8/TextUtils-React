af9775f5e56c1a9419b9cd376d37e98e93f2d8f7
GauravKumarGR8 <bsn.gauravkr1@gmail.com>
2021-10-01 12:59:03 +0530

    navbar darkMode

--- src/App.js
+++ src/App.js
@@ -2,11 +2,26 @@ import './App.css';
 // import About from './components/About';
 import Navbar from './components/Navbar';
 import TextForm from './components/TextForm';
+import React, { useState } from 'react'
 
 function App() {
+  const [mode, setMode] = useState('light') // To change mode between dark and light
+
+  const [color, setColor] = useState('dark')
+
+  const toggleMode = ()=>{
+    if(mode === 'light'){
+      setMode('dark')
+      setColor('light')
+    }
+    else{
+      setMode('light')
+      setColor('dark')
+    }
+  }
   return (
     <>
-    <Navbar title = 'TextUtils' aboutText = 'About Us'/>
+    <Navbar title = 'TextUtils' aboutText = 'About Us' mode={mode} toggleMode={toggleMode} modeTitle={color}/>
     {/* <Navbar/> */}
     <div className="container my-3" >
     <TextForm heading='Enter the text to analyze below'/>
